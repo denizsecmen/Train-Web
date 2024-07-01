cb0d0894dbd6e464451ad3e6838893dd8c20f606
Deniz Seçmen <denizsecmen1240@gmail.com>
2024-07-01 21:36:01 +0300

    add all files(empty)

---
+++ Postgres-Item selector/server/middleware/auth.ts
@@ -0,0 +1,4 @@
+export default function auth(req, res, next) {
+  
+  next();
+}
---
+++ Postgres-Item selector/server/router/router.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import signUp from '../controller/signup';
+import auth from '../middleware/auth';
+import login from '../controller/login';
+let router = express.Router();
+
+router.post('/signup', signUp);
+router.post('/login', auth, login);
