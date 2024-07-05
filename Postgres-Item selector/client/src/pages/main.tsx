26b89ec1547f1b393acc85f0bf1d469237b404ea
Deniz Seçmen <denizsecmen1240@gmail.com>
2024-07-05 19:12:05 +0300

    Latout set up

--- Postgres-Item selector/client/src/pages/main.tsx
+++ Postgres-Item selector/client/src/pages/main.tsx
@@ -1,8 +1,16 @@
 import styles from './App.module.css';
+import { Button } from '@mui/material';
 export default function menu() {
   return (
     <div className={styles.main}>
-      <div className={styles.layout}></div>
+      <div className={styles.layout}>
+      <div className={styles.flex}></div>
+        <Button variant='contained' color="secondary" className={styles.signup} sx={{ mr: '25px' }}>Sign Up</Button>
+      <Button variant='contained' color='primary' className={styles.login}>Login</Button>
+      </div>
+      <div className={styles.area}>
+        
+      </div>
     </div>
   )
 }
--- Postgres-Item selector/server/router/router.ts
+++ Postgres-Item selector/server/router/router.ts
@@ -3,9 +3,11 @@ import signUp from '../controller/signup';
 import auth from '../middleware/auth';
 import login from '../controller/login';
 import dashboard from '../controller/dashboard';
+import main from '../controller/main';
 let router = express.Router();
 
 router.post('/signup', signUp);
 router.post('/login', login);
-router.post('/dashboard', auth,dashboard);
+router.post('/dashboard', auth, dashboard);
+router.get('/', main);
 export default router;
