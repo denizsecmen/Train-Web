8f2c0628d7d3c986f4a84f535a5eeaca477704f5
Deniz Seçmen <denizsecmen1240@gmail.com>
2024-07-16 13:14:53 +0300

    Login finished.

--- Postgres-Item selector/client/src/pages/Login.tsx
+++ Postgres-Item selector/client/src/pages/Login.tsx
@@ -1,16 +1,29 @@
-import { Button, TextField, Input } from "@mui/material";
+import React from 'react';
+import { Button, TextField } from '@mui/material';
 import styles from './styles/Login.module.css';
-function BasicExample() {
+
+function Login() {
+
   return (
     <div className={styles.area}>
       <div className={styles.login}>
         <div className={styles.title}>Login</div>
         <div className={styles.body}>
-
+          <div className={styles.textField}>
+            <label>Email:</label>
+            <TextField variant="outlined" sx={{width:'100%'}} />
+            <label>Password:</label>
+            <TextField variant="outlined" sx={{width:'100%'}} fullWidth />
+          </div>
+          <div className={styles.buttons}>
+            <Button variant='contained' color="primary" className={styles.signup}>Login</Button>
+            <div className={styles.flex}/>
+            <Button variant='contained' color="secondary" className={styles.signup} >Reset</Button>
+          </div>
         </div>
       </div>
     </div>
   );
 }
 
-export default BasicExample;
+export default Login;
